e9daec4dcc09cca679b74f14518827471364ddf2
Olivier Nzia <ndjikenzia.o@northeastern.edu>
2025-10-17 08:36:54 -0400

    only show peel button when board is valid

--- packages/client/src/app/game/_components/peel.tsx
+++ packages/client/src/app/game/_components/peel.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import useGameStateContext from "@/hooks/gameState";
+import { isSingleValidComponent } from "@/utils/gameUtils";
 
 export default function Peel() {
-  const { peel } = useGameStateContext();
+  const { peel, board, wallet } = useGameStateContext();
+  const canpeel = isSingleValidComponent(board) && wallet.length == 0;
 
   return (
-    <button onClick={peel} className="fixed bottom-5 right-1/2 w-75 translate-x-1/2 h-10 rounded-2xl bg-neutral-300 opacity-75">
-      Peel (or press spacebar)
-    </button>
+    canpeel && (
+      <button
+        onClick={peel}
+        className="fixed bottom-5 right-1/2 w-75 translate-x-1/2 h-10 rounded-2xl bg-emerald-500 opacity-75 hover:bg-emerald-600 cursor-pointer transition-colors duration-300 animate-bounce"
+      >
+        click here to peel (or press spacebar)
+      </button>
+    )
   );
 }
